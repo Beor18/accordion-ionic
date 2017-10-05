d06627e29f29e1493a4a1fbc2e26578aa317b70e
Fernando Lopez <fer.ragnar19@gmail.com>
2017-10-04 21:00:03 -0300

    hacemos una peticion get
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
@@ -17,6 +18,7 @@ import { AccordionComponent } from '../components/accordion/accordion';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
@@ -8,9 +9,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  users: any[] = [];
 
-
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: Http) {
+    this.http.get("http://jsonplaceholder.typicode.com/users").subscribe((data) => {
+      
+      //Json Users
+      this.users = data.json();
+      console.log(this.users);
+    })
 
   }
 
