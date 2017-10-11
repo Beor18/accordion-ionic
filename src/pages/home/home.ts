1f314054235fd1d5fd26a907cba0c7c78175fa2e
Fernando Lopez <fer.ragnar19@gmail.com>
2017-10-11 12:57:02 -0300

    Cambios home
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,29 @@
 import { Component } from '@angular/core';
-
+import { Injectable } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Http } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+//import { Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+@Injectable()
 export class HomePage {
 
-  users: any[] = [];
+  public options;
+  public headers: Headers;
+  posts:any;
 
-  constructor(public navCtrl: NavController, private http: Http) {
-    this.http.get("http://jsonplaceholder.typicode.com/users").subscribe((data) => {
-      
-      //Json Users
-      this.users = data.json();
-      console.log(this.users);
-    })
+  constructor(public navCtrl: NavController, public http:Http) {
 
+    
+    this.http.get("http://www.flybynet.org/wp-json/wp/v2/posts", this.options).map((res:Response) => res.json()).subscribe(posts => {
+      console.log(posts);
+      this.posts = posts;
+    });
   }
 
 }
